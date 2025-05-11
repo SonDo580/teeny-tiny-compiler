d06d555e8bb1d4b75d1566cc83cc47a9c3b92245
Son Do <dominhson580@gmail.com>
2025-05-11 17:39:13 +0700

    Add TeenyTiny sample

---
+++ teenytiny.py
@@ -0,0 +1,16 @@
+from lex import Lexer
+from teenytiny_token import TokenType
+
+
+def main():
+    source = "IF+-123 foo*THEN/"
+    lexer = Lexer(source)
+
+    token = lexer.get_token()
+    while token.type != TokenType.EOF:
+        print(token.type)
+        token = lexer.get_token()
+
+
+if __name__ == "__main__":
+    main()
